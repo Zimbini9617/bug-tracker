74e9fc50ea4aa6577119dd81efce6cb5241c0a24
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-10-27 15:21:07 +0200

    handle form submission update

--- app/api/bugs/new/page.tsx
+++ app/api/bugs/new/page.tsx
@@ -3,15 +3,33 @@ import React from 'react';
 import { TextField, TextArea, Button } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+interface BugForm{
+  title: string;
+  description: string;
+}
+
 const NewBugPage = () => {
+  const {register, control, handleSubmit} = useForm<BugForm>();
+  const router = useRouter();
+  console.log(register('title'))
   return (
-    <div className='max-w-xl space-y-3'>
+    
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data)=>{
+      await axios.post('/api/bugs', data);
+      router.push('/bugs')
+    })}>
       <TextField.Root>
-        <TextField.Input placeholder='title' />
+        <TextField.Input placeholder='title' {...register('title')}/>
       </TextField.Root>
-      <SimpleMDE placeholder='Description' />
+      <Controller control={control} name='description' render={({field})=> (<SimpleMDE placeholder='Description' {...field}/>)}/>
+      
       <Button>SUBMIT NEW BUG</Button>
-    </div>
+    </form>
+
   )
 }
 
