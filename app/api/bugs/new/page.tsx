58f65868b36e28077bde89c88b8489d9b92f5ecf
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-10-27 14:26:07 +0200

    markdown setup

--- app/api/bugs/new/page.tsx
+++ app/api/bugs/new/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 import React from 'react';
 import { TextField, TextArea, Button } from '@radix-ui/themes';
-
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 const NewBugPage = () => {
   return (
     <div className='max-w-xl space-y-3'>
       <TextField.Root>
         <TextField.Input placeholder='title' />
       </TextField.Root>
-      <TextArea placeholder='Description' />
+      <SimpleMDE placeholder='Description' />
       <Button>SUBMIT NEW BUG</Button>
     </div>
   )
