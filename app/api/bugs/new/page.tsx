ba944840fc7e4538180c28616083e9485ec06544
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-10-29 17:25:48 +0200

    Handling errors

--- app/api/bugs/new/page.tsx
+++ app/api/bugs/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React from 'react';
-import { TextField, TextArea, Button } from '@radix-ui/themes';
+import React, {useState} from 'react';
+import { TextField, TextArea, Button, Callout } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
@@ -15,12 +15,21 @@ interface BugForm{
 const NewBugPage = () => {
   const {register, control, handleSubmit} = useForm<BugForm>();
   const router = useRouter();
-  console.log(register('title'))
+  const [error, setError] = useState('');
   return (
-    
-    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data)=>{
-      await axios.post('/api/bugs', data);
+    <div className='max-w-xl'>
+    {error && (<Callout.Root className='mb-5'>
+      <Callout.Text color='green'>{error}</Callout.Text>
+      </Callout.Root>)}
+    <form className='space-y-3' onSubmit={handleSubmit(async (data)=>{
+      try{
+        throw new Error();
+        await axios.post('/api/bugs', data);
       router.push('/bugs')
+      }
+      catch(error){
+        setError('Unexpected Error happened')
+      }
     })}>
       <TextField.Root>
         <TextField.Input placeholder='title' {...register('title')}/>
@@ -29,7 +38,7 @@ const NewBugPage = () => {
       
       <Button>SUBMIT NEW BUG</Button>
     </form>
-
+    </div>
   )
 }
 
--- app/api/bugs/route.ts
+++ app/api/bugs/route.ts
@@ -12,7 +12,7 @@ const body = await request.json();
 const validation = createBugSchema.safeParse(body)
 
 if(!validation.success)
-return NextResponse.json(validation.error.errors, {status: 400});
+return NextResponse.json(validation.error.format(), {status: 400});
 
 const newBug = await prisma.bug.create({
   data: {
