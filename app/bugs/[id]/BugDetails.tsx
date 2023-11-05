6e7b6810504244a52bb759b7ec29dcdf8145a927
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-11-05 16:14:30 +0200

    applying Single Responsibility Principle

---
+++ app/bugs/[id]/BugDetails.tsx
@@ -0,0 +1,21 @@
+import { BugStatusBadge } from '@/app/components'
+import { Heading, Flex, Card } from '@radix-ui/themes'
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+
+const BugDetails = ({ bug }:{ bug: Bug }) => {
+  return (
+    <>
+    <Heading>{bug.title}</Heading>
+        <Flex gap='4' my='4'>
+          <BugStatusBadge status={bug.status} />
+          <p>{bug.createdAt.toDateString()}</p>
+        </Flex>
+        <Card className='prose' my='4'>
+          <ReactMarkdown>{bug.description}</ReactMarkdown>
+        </Card>
+        </>
+  )
+}
+
+export default BugDetails
--- app/bugs/[id]/page.tsx
+++ app/bugs/[id]/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import prisma from '@/prisma/client';
 import { notFound } from 'next/navigation';
-import { Heading, Flex, Card, Grid, Box, Button } from '@radix-ui/themes';
+import { Heading, Flex, Card, Grid, Box } from '@radix-ui/themes';
 import BugStatusBadge from '../../components/BugStatusBadge';
 import ReactMarkdown from 'react-markdown';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import Link from 'next/link';
+import EditBugButton from './EditBugButton';
+import BugDetails from './BugDetails';
 
 interface Props {
   params: { id: string };
@@ -20,23 +20,13 @@ const BugDetailsPage = async ({ params }: Props) => {
   return (
     <Grid columns={{initial: '1', md: '2'}} gap='4'>
       <Box>
-      <Heading>{bug.title}</Heading>
-      <Flex gap='4' my='4'>
-      <BugStatusBadge status={bug.status} />
-      <p>{bug.createdAt.toDateString()}</p>
-      </Flex>
-      <Card className='prose' my='4'>
-      <ReactMarkdown>{bug.description}</ReactMarkdown>
-      </Card>
+        <BugDetails bug = {bug}/>
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/bugs/edit/${bug.id}`}>UPDATE BUG</Link>
-        </Button>
+        <EditBugButton bugId={bug.id}/>
       </Box>
     </Grid>
-  )
-}
+  );
+};
 
 export default BugDetailsPage;
