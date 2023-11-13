626bf925e670f0d1324e2285d4e1ff023ad9e18f
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-11-13 14:09:22 +0200

    fetch data with reactQuery

--- app/bugs/[id]/AssigneeSelect.tsx
+++ app/bugs/[id]/AssigneeSelect.tsx
@@ -1,37 +1,40 @@
-'use client'
-import { Select } from '@radix-ui/themes';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+'use client';
 import { User } from 'next-auth';
-
+import { Select } from '@radix-ui/themes';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import Skeleton from '@/app/components/Skeleton';
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+const {
+data: users,
+error,
+isLoading,
+} = useQuery<User[]>({
+queryKey: ['users'],
+queryFn: () => axios.get('/api/users').then((res) => res.data),
+staleTime: 60 * 1000,
+retry: 3,
+});
+if (isLoading) return <Skeleton />;
+if (error) return null;
 
-  useEffect(()=>{
-    const fetchUsers = async ()=>{
-      const {data} = await axios.get<User[]>('/api/users');
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
-
-  return (
-  <Select.Root>
-    <Select.Trigger placeholder='Assign...' />
-    <Select.Content>
-      <Select.Group>
-        <Select.Label>Suggestions</Select.Label>
-        {users.map((user)=>(
-           <Select.Item key={user.id} value={user.id}>
-            {user.name}
-           </Select.Item>
-        ))}
-       
-      </Select.Group>
-    </Select.Content>
-  </Select.Root>
-  );
+return (
+<Select.Root>
+<Select.Trigger placeholder="Assign..." />
+<Select.Content>
+<Select.Group>
+<Select.Label>Suggestions</Select.Label>
+{users?.map((user) => (
+<Select.Item key={user.id} value={user.id}>
+{user.name}
+</Select.Item>
+))}
+</Select.Group>
+<Select.Separator />
+</Select.Content>
+</Select.Root>
+);
 };
 
 export default AssigneeSelect;
