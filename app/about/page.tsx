60fa91f5fa4a0e7a3bd725c8facf4c842e45f307
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-11-24 13:24:04 +0200

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div className=''>
-      <h1 className='flex mx-auto text-center text-[#425E5B] font-bold mb-6 text-3xl'>WE PUT OUR CUSTOMERS NEEDS FIRST</h1>
+    <>
+    <div className='flex flex-row'>
+      <h1 className='mx-auto text-center text-[#425E5B] font-bold mb-6 text-3xl'>WE PUT OUR CUSTOMERS NEEDS FIRST</h1>
 
       <div className='text-[#425E5B]'>
         <h2 className='font-bold text-2xl'>OUR VISION</h2>
         <p className='text-xl'>Looking to the future, <br /> we see a world where<br /> everyone is free of <span className='font-bold text-red-800'>BUGS!</span></p>
       </div>
-  
     </div>
+
+    <div>
+      <p className='text-[#425E5B] text-xl mt-10'>
+        We are passionate about helping our customers to stay free of bugs on their devices.<br />
+        We are excited to play a huge role in bringing faster way to fix bugs, in a more affordable<br /> way to all communities.<br />
+        We promise to give the best services as possible to our beloved customers.
+      </p>
+    </div>
+    </>
   )
 }
 
