30dfa7f0ff209393ec52fc92659b5baccdf698f8
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-11-24 12:13:28 +0200

    About Page update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className=''>
+      <h1 className='flex mx-auto text-center text-[#425E5B] font-bold mb-6 text-3xl'>WE PUT OUR CUSTOMERS NEEDS FIRST</h1>
+
+      <div className='text-[#425E5B]'>
+        <h2 className='font-bold text-2xl'>OUR VISION</h2>
+        <p className='text-xl'>Looking to the future, <br /> we see a world where<br /> everyone is free of <span className='font-bold text-red-800'>BUGS!</span></p>
+      </div>
+  
+    </div>
   )
 }
 
