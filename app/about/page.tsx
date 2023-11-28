1d02fb22514675f817fe27455be335d171ccd01b
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-11-28 11:25:05 +0200

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const AboutPage = () => {
   return (
     <>
     <div className='flex flex-row'>
-      <h1 className='mx-auto text-center text-[#425E5B] font-bold mb-6 text-3xl'>WE PUT OUR CUSTOMERS NEEDS FIRST</h1>
+      <h1 className='mx-auto text-center text-[#425E5B] font-bold font-mono mb-6 text-3xl'>WE PUT OUR CUSTOMERS NEEDS FIRST</h1>
 
       <div className='text-[#425E5B]'>
-        <h2 className='font-bold text-2xl'>OUR VISION</h2>
+        <h2 className='font-bold text-2xl font-mono'>OUR VISION</h2>
         <p className='text-xl'>Looking to the future, <br /> we see a world where<br /> everyone is free of <span className='font-bold text-red-800'>BUGS!</span></p>
       </div>
     </div>
